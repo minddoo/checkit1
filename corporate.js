da13a64c61da8233193c5c19199db836e18eae6d
xhrlalswjd7989 <xhrlalswjd7989@gmail.com>
2026-02-23 13:34:26 +0000

    fix: 기업 고객 페이지 스크립트 오류 수정 및 콘텐츠 재적용

--- corporate.js
+++ corporate.js
@@ -160,7 +160,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
         const elements = document.querySelectorAll("[data-i18n]");
         elements.forEach(el => {
-            const key = el.getAttribute("data-i18n]");
+            const key = el.getAttribute("data-i18n");
             if (translations[lang] && translations[lang][key]) {
                 el.innerHTML = translations[lang][key];
             }
